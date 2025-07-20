1132233de5b37e966c41fcad041456a994bd16ca
asif-iqbal-fin <asif.iqbal.fin@gmail.com>
2025-07-20 19:24:59 +0530

    Ex 2.4

--- part2/collection/src/App.jsx
+++ part2/collection/src/App.jsx
@@ -1,27 +1,53 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-return <Course course={course} />
+  return <div>
+    <h1>Web development curriculum</h1>
+    {courses.map(course => <Course course={course} />)}
+  </div>
 }
 
 const Course = ({course}) => {
@@ -37,7 +63,7 @@ const Course = ({course}) => {
 const Header = ({coursename}) =>{
   return (
     <div>
-      <h1>{coursename}</h1>
+      <h2>{coursename}</h2>
     </div>
   )
 }
@@ -68,7 +94,7 @@ const Total = ({parts}) =>{
   },0)
   return(
     <div>
-      <p>Number of exercises {totalValue}</p>
+      <b>Number of exercises {totalValue}</b>
     </div>
   )
 }
