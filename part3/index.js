4a5d3d2849ae93af277154db922ddc0934ed2105
asif-iqbal-fin <asif.iqbal.fin@gmail.com>
2025-07-28 15:21:27 +0530

    Ex 3.13

--- part3/index.js
+++ part3/index.js
@@ -1,49 +1,18 @@
-import express from 'express'
-import morgan from 'morgan'
-import uuid from 'node-uuid'
-
-morgan.token('id', (req) =>{
-    return req.id
-})
-
-const assignId = (req,res, next) =>{
-    req.id = uuid.v4()
-    next()
-}
+require('dotenv').config()
+const express = require('express')
+const Person = require('./models/person')
 
 const app = express()
 
-var persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.use(express.static('dist'))
 app.use(express.json())
-app.use(assignId)
-app.use(morgan(':id :method :url :response-time'))
+
 
 app.get('/api/persons',(request,response) => {
     console.log('Successfuully fetched persons JSON')
-    response.send(persons)
+    Person.find({}).then(persons =>{
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request,response) => {
@@ -97,7 +66,7 @@ app.post('/api/persons', (request,response) => {
     response.json(newPerson)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ part3/models/person.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+    .then(
+    result => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('error opening the MongoDB',error.message)
+    })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+module.exports = mongoose.model('Person',personSchema)
