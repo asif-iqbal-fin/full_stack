540956578b961fdc4ff5c29fd5b0a6ae587c1baa
asif-iqbal-fin <asif.iqbal.fin@gmail.com>
2025-08-31 19:16:14 +0530

    Ex 13.14

--- part13-1/controllers/blogs.js
+++ part13-1/controllers/blogs.js
@@ -23,13 +23,17 @@ const tokenExtractor = (req,res,next) => {
 }
 
 router.get('/', async (req,res) => {
-    const where = {}
+    var where = {}
 
     if(req.query.search){
-        where.title = {
-            [Op.substring]: req.query.search
+        where = {
+            [Op.or]: [
+                {title: {[Op.substring]: req.query.search}},
+                {author: {[Op.substring]: req.query.search}},
+            ]
         }
     }
+
     const blogs = await Blog.findAll({
         attributes: {exclude: ['userId']},
         include: {
