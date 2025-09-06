c128dbbb869dc816c636de3c970a961b2fcd5d45
asif-iqbal-fin <asif.iqbal.fin@gmail.com>
2025-09-06 11:42:11 +0530

    file name of filter rducer corrected - Ex 6.9

--- part6/redux-anecdotes/src/components/Filter.jsx
+++ part6/redux-anecdotes/src/components/Filter.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux"
-import { filterChange } from "../reducers/anecdoteFilter"
+import { filterChange } from "../reducers/filterReducer"
 
 const Filter = () => {
     const dispatch = useDispatch()
--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -3,7 +3,7 @@ import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/anecdoteFilter'
+import filterReducer from './reducers/filterReducer'
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
