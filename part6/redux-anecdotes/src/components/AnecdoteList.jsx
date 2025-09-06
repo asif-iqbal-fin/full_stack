e1ef5f68e82b6b9739b1cf7d637ed926c193629a
asif-iqbal-fin <asif.iqbal.fin@gmail.com>
2025-09-06 20:06:22 +0530

    Ex 6.13

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,5 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { voteFor } from '../reducers/anecdoteReducer'
+import { removeNotification, setNotification } from '../reducers/notificationReducer'
+
 
 const AnecdoteList = () => {
 
@@ -12,6 +14,16 @@ const AnecdoteList = () => {
     const vote = (id) => {
         console.log('vote', id)
         dispatch(voteFor(id))
+
+        //filter the anecdote by id 
+        const anecdoteTitle = anecdotes.filter(anecdote => anecdote.id === id)[0].content
+        dispatch(setNotification(`Voted for - ${anecdoteTitle}`))
+        
+        //wait for 5 second
+        setTimeout(() => {
+          //remove the notification
+        dispatch(removeNotification(''))
+        }, 5000)
       }
 
 
--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -5,7 +5,16 @@ const initialState = 'The Notification shows up here !!'
 const notificationSlice = createSlice({
     name: 'notifcation',
     initialState,
-    reducers: {}
+    reducers: {
+        setNotification(state,action){
+            return action.payload
+        },
+        removeNotification(state,action){
+            return action.payload
+        }
+    }
 })
+
+export const { setNotification, removeNotification} = notificationSlice.actions
 
 export default notificationSlice.reducer
